09c646f8144203583d148c2ba1ff79167b6c54bb
jhlav1 <jhlav1@hotmail.com>
2022-10-04 21:01:22 +0200

    commit

---
+++ script3.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("hello").innerHTML = "Goodby World";
+}
+
+function changeColor() {
+    document.getElementById("hello").style.color = "Red";
+}
